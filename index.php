3c35eea29a4dd46d1a551f54326770fe6948b27c
ydairz <60136627+ydairz@users.noreply.github.com>
2021-09-18 00:07:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
         <link rel="stylesheet" href="./css/transition.css">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css">
     </head>
-    <body onload="loadFunction()">
+    <body>
         <?php include "./navbar.php"; ?>
         <main>
             <?php
